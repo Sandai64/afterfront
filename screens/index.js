9a147802ad7862f099dce11d78d5f715c0c9ae9b
ValentinJDT <vjeandot@yahoo.com>
2022-04-11 15:13:24 +0200

    add user screen and exports

--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,15 @@
 import DefaultScreen from './DefaultScreen';
 import LoginScreen from './authentication/LoginScreen';
 import WelcomeScreen from './loading/WelcomeScreen';
+import OrdersScreen from './orders/OrdersScreen';
+import OrderDetailScreen from './orders/OrderDetailScreen';
+import UserScreen from './user/UserScreen';
 
 export {
   DefaultScreen,
   LoginScreen,
-  WelcomeScreen
+  WelcomeScreen,
+  OrdersScreen,
+  OrderDetailScreen,
+  UserScreen
 }
