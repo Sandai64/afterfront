4b748cf561314275fedf817185b45ba0dd2eae37
ValentinJDT <vjeandot@yahoo.com>
2022-04-11 15:12:50 +0200

    add orders screens

---
+++ screens/orders/OrdersScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import GlobalStyles from '../../config/styles';
+
+const OrdersScreen = () => {
+    return (
+        <View style={style.container}>
+            <Text>OrdersScreen</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: GlobalStyles.color.primary,
+        height: "100%"
+    }
+});
+
+export default OrdersScreen;
