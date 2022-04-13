df583e078d44409ffcbde2ca660ed7f94fcadd58
ValentinJDT <vjeandot@yahoo.com>
2022-04-13 14:51:42 +0200

    add nav animation

--- screens/loading/WelcomeScreen.js
+++ screens/loading/WelcomeScreen.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, Image, View, Animated } from 'react-native';
+
 import GlobalStyles from '../../config/styles';
 import logo from '../../static/img/logos/logo-full.png';
 import { useFonts } from 'expo-font';
 
 const WelcomeScreen = ({ navigation }) => {
+
+  const wait = 2200;
+  const duration = 500;
+
   let [fontsLoaded] = useFonts({
     Inter_Regular: require('../../static/fonts/Inter-Regular.ttf'),
   });
@@ -12,12 +17,32 @@ const WelcomeScreen = ({ navigation }) => {
   useEffect(() => {
     setTimeout(() => {
       navigation.replace('Logged');
-    }, 2500);
+    }, (wait+50));
   }, [fontsLoaded]);
+
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    if(fontsLoaded) {
+      setTimeout(() => {
+        Animated.timing(
+          fadeAnim,
+          {
+            toValue: 0,
+            duration: duration,
+            useNativeDriver: true,
+          }
+        ).start();
+      }, (wait-duration));
+    }
+  }, [fadeAnim, fontsLoaded]);
 
   return (
     <View style={style.container}>
-      <Image source={logo} style={style.logo} />
+      <Animated.Image
+        source={logo}
+        style={{ width: '45%', height: '45%', opacity: fadeAnim }}
+      />
     </View>
   );
 };
