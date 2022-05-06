22d584f68af7b3f784730ad580a56dddc57a1cd7
ValentinJDT <vjeandot@yahoo.com>
2022-05-06 17:31:16 +0200

    add ProductItem export

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import OrderItem from "./orders/OrderItem";
 import MoreIcon from "./orders/MoreIcon";
+import ProductItem from "./orders/ProductItem";
 
 export {
     OrderItem,
+    ProductItem,
     MoreIcon
 }
