772447e5b2602a42046fce677666046746f27c49
ValentinJDT <vjeandot@yahoo.com>
2022-05-07 15:13:04 +0200

    remove more icon

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 import OrderItem from './orders/OrderItem';
-import MoreIcon from './orders/MoreIcon';
 import ProductItem from './orders/ProductItem';
 import Loader from './loader/Loader';
 
-export { OrderItem, ProductItem, MoreIcon, Loader };
+export { OrderItem, ProductItem, Loader };
