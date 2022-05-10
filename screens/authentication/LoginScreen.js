fca15aa2348c8b41121d3fe2802463fcb4cc03e6
Erwan Egasse <contact@erwan.sh>
2022-05-10 17:39:19 +0200

    Rebase against main

--- screens/authentication/LoginScreen.js
+++ screens/authentication/LoginScreen.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import GlobalStyles from '../../config/styles';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: GlobalStyles.color.primary,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: 64,
+    paddingLeft: 18,
+    paddingRight: 18,
+  },
+  pageHeader: {
+    borderBottomColor: 'black',
+    marginBottom: 24,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderRightWidth: 0,
+    borderLeftWidth: 0,
+    width: 'fit-content',
+    fontSize: 28,
+  },
+});
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Login page</Text>
+    <View style={styles.container}>
+      <Text style={styles.pageHeader}>Connexion</Text>
+      <Button color="#261F18" title="OK, allons-y !" />
     </View>
   );
 };
